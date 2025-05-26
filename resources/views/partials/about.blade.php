b4774ce2a15c5b995cf32fd0c54ed066b9fea0df
ouedgv@hotmail.fr <ouedgv@hotmail.fr>
2025-05-26 19:51:35 +0000

    partials - slogans

---
+++ resources/views/partials/about.blade.php
@@ -0,0 +1,32 @@
+<div class="about_wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12">
+                <div class="abt_img abt_box">
+                    <img src="images/abt_img.jpg" alt="img">
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 abt_section">
+                <div class="abt_heading_wrapper">
+                    <h1 class="med_bottompadder20">about medical</h1>
+                    <img src="images/line.png" alt="title" class="med_bottompadder20">
+                </div>
+                <div class="abt_txt">
+                    <h3>Specialty Medicine with Compassion and Car Proin gravida nibh vel velit</h3>
+                    <p class="med_toppadder20">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, aks lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate..</p>
+                    <p>Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit.</p>
+                </div>
+                <div class="abt_chk med_toppadder30">
+                    <div class="content">
+                        <ul>
+                            <li><i class="fa fa-check-square-o" aria-hidden="true"></i><span>Health Care For All Types</span></li>
+                            <li><i class="fa fa-check-square-o" aria-hidden="true"></i><span>Emergency Services</span></li>
+                            <li><i class="fa fa-check-square-o" aria-hidden="true"></i><span>Dental Care Dentist.</span></li>
+                            <li><i class="fa fa-check-square-o" aria-hidden="true"></i><span>24/7 Services</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
