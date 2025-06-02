971274b32d70ff04b9927927990e561b47062b4e
flashives <jeaniveskoeta@gmail.com>
2025-06-02 22:37:39 +0000

    modifs

--- resources/views/pages/index_clinique.blade.php
+++ resources/views/pages/index_clinique.blade.php
@@ -804,13 +804,13 @@
          <div class="row">
              <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                  <div class="vedio_heading_wrapper wow fadeInDown" data-wow-delay="0.5s">
-                     <h1 class="med_bottompadder20">Welcome to Hospital</h1>
+                     <h1 class="med_bottompadder20">BIENVENUE </h1>
                      <img src="images/Icon_team.png" alt="line" class="med_bottompadder20">
-                     <p>We Care about everything what you want</p>
-                     <h4><a class="popup-youtube" href="https://www.youtube.com/embed/xImpyYRVGOc"><img src="images/Play-Icon.png" alt="Play"> play video</a></h4>
+                     <p>La santé d'abord</p>
+                     <h4><a class="popup-youtube" href="https://www.youtube.com/embed/xImpyYRVGOc"><img src="images/Play-Icon.png" alt="Play"> video</a></h4>
                      <div class="video_btn_wrapper right">
                          <ul>
-                             <li><a class="btn" href="#">About Us</a></li>
+                             <li><a class="btn" href="#">A propos de nous</a></li>
                          </ul>
                      </div>
                  </div>
@@ -1000,7 +1000,7 @@
                          </div>
                          <div class="count-description">
                              <span class="timer">2340</span>
-                             <h5 class="con1">Satisfied Patients</h5>
+                             <h5 class="con1">Patients satisfaits</h5>
                          </div>
                      </div>
                      <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
@@ -1009,7 +1009,7 @@
                          </div>
                          <div class="count-description">
                              <span class="timer">335</span>
-                             <h5 class="con2">doctor's team</h5>
+                             <h5 class="con2">Membres du corps médical</h5>
                          </div>
                      </div>
                      <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
@@ -1018,7 +1018,7 @@
                          </div>
                          <div class="count-description">
                              <span class="timer">1305</span>
-                             <h5 class="con2">success mission</h5>
+                             <h5 class="con2">Missions réussies</h5>
                          </div>
                      </div>
                      <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
@@ -1027,7 +1027,7 @@
                          </div>
                          <div class="count-description">
                              <span class="timer">1540</span>
-                             <h5 class="con4">Successfull Surgeries</h5>
+                             <h5 class="con4">Chirurgies réussies</h5>
                          </div>
                      </div>
                  </div>
